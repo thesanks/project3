a424444ddb4d59daefb081871a8aecdc73375d73
Macbook <apple@Apples-MacBook-Pro.local>
2018-07-14 21:18:07 +0100

    set up db with sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration  # import our configuration data.
 
 app = Flask(__name__)
 app.config.from_object(Configuration)  # use values from our Configuration object.
+db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -1,2 +1,5 @@
+import os
 class Configuration(object):
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
