74d4a01bd339c5662b37337624f92378716fadb2
Macbook <apple@Apples-MacBook-Pro.local>
2018-07-14 23:30:24 +0100

    added tagging model migrated db

---
+++ manage.py
@@ -0,0 +1,8 @@
+# script manage to run migrations
+# check if this is similar to django manage.py
+
+from app import manager
+from main import *
+
+if __name__ == '__main__':
+    manager.run()
--- models.py
+++ models.py
@@ -5,14 +5,25 @@ from app import db
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
 
+# don't need a model...create a table for mapping Entry to Tag
+entry_tags = db.Table('entry_tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+    db.Column('entry_id', db.Integer, db.ForeignKey('entry.id'))
+)
+
 class Entry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    slug = db.Column(db.String(100), unique=True)
-    body = db.Column(db.Text)
-    created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+    STATUS_PUBLIC = 0
+    STATUS_DRAFT = 1
+
+    id     = db.Column(db.Integer, primary_key=True)
+    title  = db.Column(db.String(100))
+    slug   = db.Column(db.String(100), unique=True)
+    body   = db.Column(db.Text)
+    status = db.Column(db.SmallInteger, default=STATUS_PUBLIC)
+    created_timestamp  = db.Column(db.DateTime, default=datetime.datetime.now)
     modified_timestamp = db.Column(db.DateTime,default=datetime.datetime.now, onupdate=datetime.datetime.now)
 
+    tags = db.relationship('Tag', secondary=entry_tags, backref=db.backref('entries', lazy='dynamic'))
     # sets slug when new model created
     def __init__(self, *args, **kwargs):
         super(Entry, self).__init__(*args, **kwargs)
@@ -22,6 +33,19 @@ class Entry(db.Model):
         self.slug = ''
         if self.title:
             self.slug = slugify(self.title)
-        
+
     def __repr__(self):
         return '<Entry: {}'.format(self.title)
+
+
+class Tag(db.Model):
+    id   = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag {}>'.format(self.name)
