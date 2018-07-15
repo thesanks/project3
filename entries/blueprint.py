fae148e8c76e8ac51411c7afce4a43e38bebcdd2
Macbook <apple@Apples-MacBook-Pro.local>
2018-07-15 01:33:28 +0100

    blueprints, views, templates

---
+++ entries/blueprint.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+from flask import render_template
+
+from helpers import object_list
+from models import Entry, Tag
+
+entries = Blueprint('entries', __name__, template_folder='templates')
+
+@entries.route('/')
+def index():
+    entries = Entry.query.order_by(Entry.created_timestamp.desc())
+    return object_list('entries/index.html', entries)
+
+@entries.route('/tags/')
+def tag_index():
+    tags = Tag.query.order_by(Tag.name)
+    return object_list('entries/tag_index.html', tags)
+
+@entries.route('/tags/<slug>/')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first_or_404()
+    entries = tag.entries.order_by(Entry.created_timestamp.desc())
+    return object_list('entries/tag_detail.html', entries, tag=tag)
+
+@entries.route('/<slug>/')
+def detail(slug):
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    return render_template('entries/detail.html', entry=entry)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app import app, db
+from entries.blueprint import entries
+
 import models
 import views
+
+app.register_blueprint(entries, url_prefix='/entries')
 
 if __name__ == '__main__':
     app.run()
