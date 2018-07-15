bcafb8f9565b52654dda84d6699ad3aae524329b
Macbook <apple@Apples-MacBook-Pro.local>
2018-07-15 22:18:10 +0100

    added delete function

--- entries/blueprint.py
+++ entries/blueprint.py
@@ -13,7 +13,7 @@ def index():
     return entry_list('entries/index.html', entries)
 
 
-@entries.route('/create/')
+# @entries.route('/create/')
 @entries.route('/create/', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
@@ -26,6 +26,31 @@ def create():
     else:
         form = EntryForm()
     return render_template('entries/create.html', form=form)
+
+
+@entries.route('/<slug>/edit/', methods=['GET', 'POST'])
+def edit(slug):
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    if request.method == 'POST':
+        form = EntryForm(request.form, obj=entry)
+        if form.validate():
+            entry = form.save_entry(entry)
+            db.session.add(entry)
+            db.session.commit()
+            return redirect(url_for('entries.detail', slug=entry.slug))
+    else:
+        form = EntryForm(obj=entry)
+    return render_template('entries/edit.html', entry=entry, form=form)
+
+@entries.route('/<slug>/delete/', methods=['GET', 'POST'])
+def delete(slug):
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    if request.method == 'POST':
+        entry.status = Entry.STATUS_DELETED
+        db.session.add(entry)
+        db.session.commit()
+        return redirect(url_for('entries.index'))
+    return render_template('entries/delete.html', entry=entry)
 
 
 @entries.route('/tags/')
