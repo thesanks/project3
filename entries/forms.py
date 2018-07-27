66303e869d6965e7c47fa5e62089dbfa0b41376a
Macbook <apple@Apples-MacBook-Pro.local>
2018-07-27 11:29:51 +0100

    sorted tag form/model to take multiple tags

--- entries/forms.py
+++ entries/forms.py
@@ -1,7 +1,37 @@
 import wtforms
 from wtforms.validators import DataRequired
-from models import Entry
+from models import Entry, Tag
 
+class TagField(wtforms.StringField):
+    def _value(self):
+        if self.data:
+            # return as a comma-separated list.
+            return ', '.join([tag.name for tag in self.data])
+        return ''
+
+    def get_tags_from_string(self, tag_string):
+        raw_tags = tag_string.split(',')
+
+        # Filter out empty tag names.
+        tag_names = [name.strip() for name in raw_tags if name.strip()]
+
+        # Query and retrieve any tags already saved.
+        existing_tags = Tag.query.filter(Tag.name.in_(tag_names))
+
+        # Which tag names are new?
+        new_names = set(tag_names) - set([tag.name for tag in existing_tags])
+
+        # Create list of unsaved Tag instances for  new tags.
+        new_tags = [Tag(name=name) for name in new_names]
+
+        # Return all existing tags + all new, unsaved tags.
+        return list(existing_tags) + new_tags
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = self.get_tags_from_string(valuelist[0])
+        else:
+            self.data = []
 
 class EntryForm(wtforms.Form):
     title = wtforms.StringField('Title', validators=[DataRequired()])
@@ -9,6 +39,7 @@ class EntryForm(wtforms.Form):
     status = wtforms.SelectField('Entry status', choices=(
             (Entry.STATUS_PUBLIC, 'Public'),
             (Entry.STATUS_DRAFT,  'Draft')), coerce=int)
+    tags = TagField('Tags', description='Separate multiple tags with commas.')
 
     def save_entry(self, entry):
         self.populate_obj(entry)
